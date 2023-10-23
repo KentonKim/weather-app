fdd3b01baa1e88fa5358246bb2ce0c9d329d6e83
Kenton Kim <109005186+KentonKim@users.noreply.github.com>
2023-10-23 15:57:46 -0700

    Added fetch function for 7 days of forecast

---
+++ src/getData.js
@@ -0,0 +1,10 @@
+import weatherapi from "./weatherapi"
+
+const getData = async (location) => {
+    const string = `https://api.weatherapi.com/v1/forecast.json?key=${weatherapi()}&q=${location}&days=7`
+    const response = await fetch(string)
+    const data = await response.json()
+    return data.forecast.forecastday
+}
+
+export default getData
--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
+import getData from './getData';
 import './style.css'
 
 console.log('hello')
-function component() {
-    const element = document.createElement('div');
- 
-    // Lodash, now imported by this script
-   element.classList.add('hello');
-   element.textContent = 'whats up'
- 
-    return element;
-  }
- 
-  document.body.appendChild(component());
+
+let days
+getData('90630')
+  .then((resolve) => {
+    console.log(resolve)
+    days = resolve
+  })
+  .catch((error) => {
+    console.log(error)
+  })
