bf86e05414b3721d955edb58d03652a6fba1688b
Kenton Kim <109005186+KentonKim@users.noreply.github.com>
2023-10-23 16:57:29 -0700

    messing w time

--- src/getData.js
+++ src/getData.js
@@ -4,7 +4,8 @@ const getData = async (location) => {
     const string = `https://api.weatherapi.com/v1/forecast.json?key=${weatherapi()}&q=${location}&days=7`
     const response = await fetch(string)
     const data = await response.json()
-    return data.forecast.forecastday
+    // return data.forecast.forecastday
+    return data
 }
 
 export default getData
--- src/index.js
+++ src/index.js
@@ -7,7 +7,12 @@ let days
 getData('90630')
   .then((resolve) => {
     console.log(resolve)
-    days = resolve
+    days = resolve.forecast.forecastday
+    console.log(days)
+    days.forEach(element => {
+      const date = new Date(element.date)
+      console.log(date, date.getUTCDate())
+    });
   })
   .catch((error) => {
     console.log(error)
