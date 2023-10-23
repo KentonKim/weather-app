6b212a655155d906afbfa5a3a559452ba65038e0
Kenton Kim <109005186+KentonKim@users.noreply.github.com>
2023-10-23 14:33:49 -0700

    Add day class Add createCard file

---
+++ src/Day.js
@@ -0,0 +1,11 @@
+export class Day {
+    constructor() {
+        this.date = 
+        this.dayOfWeek = 
+        this.dayOfWeekAbrev = 
+        this.highC = 
+        this.lowC = 
+        this.highF = 
+        this.lowF = 
+    }
+}
