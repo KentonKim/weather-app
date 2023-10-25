74239f7917aa1518e0159751a2bd578ded56e0e7
Kenton Kim <109005186+KentonKim@users.noreply.github.com>
2023-10-25 00:58:07 -0700

    Added Day class to wrap html elments and data - Added methods to display temperature either in Celcius or Fahrenheit - Added methods to display weekday long or abbreviated

--- src/Day.js
+++ src/Day.js
@@ -1,11 +1,30 @@
-export class Day {
-    constructor() {
-        this.date = 
-        this.dayOfWeek = 
-        this.dayOfWeekAbrev = 
-        this.highC = 
-        this.lowC = 
-        this.highF = 
-        this.lowF = 
+export default class Day {
+    constructor(object, element) {
+        this.data = object
+        this.element = element
+        this.tempDiv = element.getElementsByClassName('temperature')[0]
+        this.dayDiv = element.getElementsByClassName('weekday')[0]
+    }
+
+    displayTempF() {
+        const maxf = this.data.day.maxtemp_f
+        const minf = this.data.day.mintemp_f
+        this.tempDiv.textContent = `HI: ${maxf} \n LOW: ${minf}`
+    }
+
+    displayTempC() {
+        const maxc = this.data.day.maxtemp_c
+        const minc = this.data.day.mintemp_c
+        this.tempDiv.textContent = `HI: ${maxc}\nLOW: ${minc}`
+    }
+
+    displayDayLong() {
+        this.dayDiv.textContent = this.data.weekdayLong
+        return this.dayDiv
+    }
+
+    displayDayShort() {
+        this.dayDiv.textContent = this.data.weekdayShort
+        return this.dayDiv
     }
 }
