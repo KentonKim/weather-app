bf0c611447d695e0dfcc85f56e755f6a26a8b4ac
Kenton Kim <109005186+KentonKim@users.noreply.github.com>
2023-10-25 01:01:23 -0700

    Extracted parameters for inputting text to reduce coupling

--- src/dom/card.js
+++ src/dom/card.js
@@ -1,18 +1,17 @@
-export const createCard = (parentDiv, weekdayText, temperatureText) => {
+export const createCard = (parentDiv) => {
     const element = document.createElement('div')
     element.classList.add('card')
     const weekday = document.createElement('div')
     weekday.className = 'weekday'
-    weekday.textContent = weekdayText
     const temperature = document.createElement('div')
     temperature.className = 'temperature'
-    temperature.textContent = temperatureText 
 
     element.appendChild(weekday)
     element.appendChild(temperature)
     parentDiv.appendChild(element)
     return element
 }
+
 export const clearCards = (parentDiv) => {
     while (parentDiv.firstChild) {
         parentDiv.removeChild(parentDiv.lastChild);
