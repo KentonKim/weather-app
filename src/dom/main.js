b6b5df2e0e5abdf6300cdf3e0f4e62dae0de951c
Kenton Kim <109005186+KentonKim@users.noreply.github.com>
2023-10-27 15:34:43 -0700

    Redirected css from id to class for better name description - main-right -> card-container

--- src/dom/main.js
+++ src/dom/main.js
@@ -2,6 +2,7 @@ const mainDom = (parentDiv) => {
     const left = document.createElement('div')
     left.id = 'main-left'
     const right = document.createElement('div')
+    right.className = 'card-container'
     right.id = 'main-right'
 
     parentDiv.appendChild(left)
