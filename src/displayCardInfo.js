16a23392338da4fa82733d7a8e65c6084d7a3a6c
Kenton Kim <109005186+KentonKim@users.noreply.github.com>
2023-10-30 23:49:34 -0700

    Added icon to card initialization Moved setDOM to applyTransition

--- src/displayCardInfo.js
+++ src/displayCardInfo.js
@@ -1,14 +1,5 @@
 import isToggledF from "./isToggledF";
 
-const setDOM = (element, func, argsArray) => {
-    const elementRef = element 
-    elementRef.classList.add('hidden')
-    setTimeout(() => {
-        functionReturn = func.apply(null, argsArray)
-        elementRef.classList.remove('hidden')
-    }, 200);
-    return functionReturn
-}
 
 const displayDayOfWeek = (dayObj, element, isLong = true) => {
     if (isLong) {
@@ -37,52 +28,18 @@ const displayTemp = (dayObj, element, isLong = true) => {
     }
 } 
 
+const displayCardIcon = (dayObj, element) => {
+    element.style.backgroundImage = `url(http:${dayObj.day.condition.icon})`
+    element.style.backgroundPosition = "center"
+    element.style.backgroundRepeat = "no-repeat"
+}
+
 const displayCardInfo = (card, dataArray) => {
     const number = card.getAttribute('data-index')
     const dayData = dataArray[number]
     displayTemp(dayData, card.lastChild)
     displayDayOfWeek(dayData, card.firstChild)
-}
-
-// who knows what works down there
-
-
-const setDay = (dayObj) => {
-    dayObj.element.classList.add('selected')
-    dayObj.selected = true
-
-    // Weather Icon
-    const mainIcon = document.getElementById('main-icon')
-    mainIcon.src = dayObj.data.day.condition.icon
-    // Location
-    const locationData = dayObj.location
-    let locationString = `${locationData.name}`
-    if (locationData.region !== '') {
-        locationString += `, ${locationData.region}`
-    }
-    document.getElementById('location-name').textContent = locationString
-    document.getElementById('location-country').textContent = `${locationData.country}`
-
-
-    // Sunset Sunrise
-
-    return dayObj
-}
-
-
-
-const setDayOfWeekIcon = (day) => {
-    day.element.style.backgroundImage = `url(http:${day.data.day.condition.icon})`
-    day.element.style.backgroundPosition = "center"
-    day.element.style.backgroundRepeat = "no-repeat"
-}
-
-const setContent = (day) => {
-    // set icon
-}
-
-const setTab = (day) => {
-    // displays temp / precipitation / wind hourly
+    displayCardIcon(dayData, card)
 }
 
 export default displayCardInfo
---
+++ src/displayMainInfo.js
@@ -0,0 +1,29 @@
+const setDay = (dayObj) => {
+    dayObj.element.classList.add('selected')
+    dayObj.selected = true
+
+    // Weather Icon
+    const mainIcon = document.getElementById('main-icon')
+    mainIcon.src = dayObj.data.day.condition.icon
+    // Location
+    const locationData = dayObj.location
+    let locationString = `${locationData.name}`
+    if (locationData.region !== '') {
+        locationString += `, ${locationData.region}`
+    }
+    document.getElementById('location-name').textContent = locationString
+    document.getElementById('location-country').textContent = `${locationData.country}`
+
+
+    // Sunset Sunrise
+
+    return dayObj
+}
+
+const setContent = (day) => {
+    // set icon
+}
+
+const setTab = (day) => {
+    // displays temp / precipitation / wind hourly
+}
